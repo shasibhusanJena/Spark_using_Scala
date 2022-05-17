122d742d82712935c8542be74649ac22cb2caac4
shasibhusanJena <shasibhusan.dev@gmail.com>
2022-05-17 14:05:37 +0530

    Scala learning examples

---
+++ ScalaBasics/src/ScalaLearning2.scala
@@ -0,0 +1,28 @@
+object calculator {
+  def main(args: Array[String]): Unit = {
+    
+  }
+  def squareIt(x: Int): Int = x*x
+  println(squareIt(4))  
+  
+  def cubeIt(x: Int): Int = x*x*x
+  println(cubeIt(4))
+  
+  def transformInt(x: Int, f:Int => Int ): Int = {
+    f(x)
+  }
+  println(transformInt(8, squareIt))
+  println(transformInt(8, cubeIt))
+  // Anonymous  class
+  println(transformInt(2, x => x*x))
+  println(transformInt(2, x => x*x*x))
+  println(transformInt(2, x => x*x*x*x))
+  
+  def divideByTwo(x: Int ) = {
+    x/2
+  }
+  println(divideByTwo(10))
+  println(transformInt(4, x => x/2))
+  println(transformInt(4, x => {val y= x*2; y * y}))
+  
+}
---
+++ ScalaBasics/src/checkLessorValues.scala
@@ -0,0 +1,21 @@
+/*The first line of Input consists of two space separated integer size and numK, representing the number of elements in the list(N) .and the integer to be compared (K). The second line consists of N space separated integer - elements[0], elements[1] .. elements[N-1] representing the list of integer
+*/
+// 5 20
+// 10 20 30 40 50 
+import scala.io.StdIn._
+object checkLessorValues {
+  def main(args: Array[String]): Unit = {
+  val inputLine1 = readLine()
+  val inputLine1Arr = inputLine1.split(" ").map(_.toInt)
+  val inputLine2 = readLine()
+  val inputLine2Arr = inputLine2.split(" ").map(_.toInt)
+  val numK = inputLine1Arr(1)
+  var count = 0
+  for(i <- inputLine2Arr){
+    if(i <=numK){
+      count =count+1
+    }
+  }
+  print(count)
+  }
+}
