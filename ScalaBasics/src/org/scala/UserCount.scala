e75c0c19daee534e45178741820afd07ab52e7bf
shasibhusanJena <shasibhusan.dev@gmail.com>
2022-05-23 12:47:51 +0530

    added GetCountSortByValue file

--- ScalaBasics/src/org/scala/UserCount.scala
+++ ScalaBasics/src/org/scala/UserCount.scala
@@ -3,7 +3,7 @@ package org.scala
 import org.apache.spark.SparkContext
 import org.apache.log4j.Level
 import org.apache.log4j.Logger
-
+//01
 object UserCount extends App{
 	
 	Logger.getLogger("org").setLevel(Level.ERROR)
